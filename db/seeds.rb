cd24fde70fce01c9f26f023e0caf430b0b917bf8
Digital-Learner <digital.learning.journeys@gmail.com>
2014-09-15 10:56:14 +0100

    Add Users to seed data (for demonstration purposes only)
    - TODO: remove before productionising

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@ Department.create(name: 'Human Resources')
 Department.create(name: 'Finance')
 Department.create(name: 'Events')
 Department.create(name: 'Other')
+
+User.create(name: 'claire',
+            email: 'claire@example.com',
+            password: 'password',
+            password_confirmation: 'password'
+           )
+
+User.create(name: 'david',
+            email: 'david@example.com',
+            password: 'staff-password',
+            password_confirmation: 'staff-password'
+           )
