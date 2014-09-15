d614d01545dfb083cd0e3505a13ee0d5ed2781bc
Digital-Learner <digital.learning.journeys@gmail.com>
2014-09-15 14:38:20 +0100

    Display all tickets
    - link to ticket via reference_id

--- spec/features/viewing_tickets_spec.rb
+++ spec/features/viewing_tickets_spec.rb
@@ -1,16 +1,25 @@
 require 'rails_helper'
 
 describe "Viewing Tickets" do
+  let!(:ticket) { FactoryGirl.create(:ticket, name: "David Latimer",
+                                              email: "d.j.latimer@example.com",
+                                              department_id: '4',
+                                              subject: "Viewing ticket by references"
+                                    )
+                }
 
   before do
     visit '/'
     click_link "Log in"
+    fill_in "Name", with: "david"
+    fill_in "Password", with: "staff-password"
+    click_button "Log in"
   end
 
   scenario "Viewing a ticket" do
-    click_link "ABC-123-ABC-123-ABC"
+    click_link ticket.reference_id
 
-    expect(page).to have_content "A new Ticket has been created."
+    expect(page).to have_content "Subject: Viewing ticket by references"
   end
 
 end
