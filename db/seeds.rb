d0b5fbd083a4ffddb69b10895fed2bbdb7c3fa50
Digital-Learner <digital.learning.journeys@gmail.com>
2014-09-15 13:31:10 +0100

    Prevent repeating seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,23 @@
-Department.create(name: 'Marketing')
-Department.create(name: 'Information Technology')
-Department.create(name: 'Sales')
-Department.create(name: 'Human Resources')
-Department.create(name: 'Finance')
-Department.create(name: 'Events')
-Department.create(name: 'Other')
+Department.create(name: 'Marketing')              unless Department.find_by(name: 'Marketing')
+Department.create(name: 'Information Technology') unless Department.find_by(name: 'Information Technology')
+Department.create(name: 'Sales')                  unless Department.find_by(name: 'Sales')
+Department.create(name: 'Human Resources')        unless Department.find_by(name: 'Human Resources')
+Department.create(name: 'Finance')                unless Department.find_by(name: 'Finance')
+Department.create(name: 'Events')                 unless Department.find_by(name: 'Events')
+Department.create(name: 'Other')                  unless Department.find_by(name: 'Other')
 
-User.create(name: 'claire',
-            email: 'claire@example.com',
-            password: 'password',
-            password_confirmation: 'password'
-           )
+unless User.find_by(email: 'claire@example.com')
+  User.create(name: 'claire',
+              email: 'claire@example.com',
+              password: 'password',
+              password_confirmation: 'password'
+             )
+end
 
-User.create(name: 'david',
-            email: 'david@example.com',
-            password: 'staff-password',
-            password_confirmation: 'staff-password'
-           )
+unless User.find_by(email: 'david@example.com')
+  User.create(name: 'david',
+              email: 'david@example.com',
+              password: 'staff-password',
+              password_confirmation: 'staff-password'
+             )
+end
