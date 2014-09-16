60f64fde4f7db8ac1cf0af0c4862b5fd35a2bd49
Digital-Learner <digital.learning.journeys@gmail.com>
2014-09-16 11:17:23 +0100

    Display tickets in correct views
    - closed tickets (completed & cancelled)
    - on-hold
    - newly unassigned (awaiting staff but not assigned)
      no concept of assigned at this point
    - open

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,7 +3,7 @@ class Ticket < ActiveRecord::Base
   belongs_to :state
 
   before_save :downcase_email!
-  before_save :generate_reference_id
+  before_save :generate_reference_id, if: "self.reference_id.nil?"
 
   validates :name, :subject, :detail, presence: true
   validates :department, presence: true
@@ -15,6 +15,25 @@ class Ticket < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   # validates :email, uniqueness: { case_sensitive: false }
+
+
+  # scope :awaiting_customer, -> { where(State.name == 'Waiting for Customer') }
+  scope :awaiting_customer, -> do
+    joins(:state).where(states: { name: 'Waiting for Customer' })
+  end
+
+
+  scope :waiting_staff_response, -> do
+    joins(:state).where(states: { name: 'Waiting for Staff Response' })
+  end
+
+  scope :on_hold, -> do
+    joins(:state).where(states: { name: 'On Hold'} )
+  end
+
+  scope :cancelled, -> { joins(:state).where(states: { name: 'Cancelled' }) }
+
+  scope :completed, -> { joins(:state).where(states: { name: 'Completed' }) }
 
   private
     def downcase_email!
--- spec/features/viewing_tickets_spec.rb
+++ spec/features/viewing_tickets_spec.rb
@@ -8,8 +8,58 @@ describe "Viewing Tickets" do
                                               subject: "Viewing ticket by references"
                                     )
                 }
+  let!(:ticket_open) { FactoryGirl.create(:ticket, name: "Stanley D Latimer",
+                                              email: "s.d.latimer@example.com",
+                                              department_id: department.id,
+                                              subject: "Display materials"
+                                    )
+                }
+  let!(:ticket_await_staff) { FactoryGirl.create(:ticket, name: "Sarah-Jane Washington",
+                                              email: "sj.washington@example.com",
+                                              department_id: department.id,
+                                              subject: "Complaint process"
+                                    )
+                }
+  let!(:ticket_await_customer) { FactoryGirl.create(:ticket, name: "David Latimer",
+                                              email: "d.j.latimer@example.com",
+                                              department_id: department.id,
+                                              subject: "Complaint process"
+                                    )
+                }
+  let!(:ticket_on_hold) { FactoryGirl.create(:ticket, name: "Darcy Activy",
+                                              email: "da1235@example.com",
+                                              department_id: department.id,
+                                              subject: "Payments taken from wrong card"
+                                    )
+                }
+  let!(:ticket_completed) { FactoryGirl.create(:ticket, name: "Sarah-Jane Washington",
+                                              email: "sj.washington@example.com",
+                                              department_id: department.id,
+                                              subject: "Payments taken on wrong date"
+                                    )
+                }
+  let!(:ticket_cancelled) do
+    cancelled = FactoryGirl.create(:ticket, name: "David Latimer",
+                                              email: "d.j.latimer@example.com",
+                                              department_id: department.id,
+                                              subject: "Payments taken in wrong currency"
+                                    )
+    cancelled.state = State.find_by(name: "Cancelled")
+    cancelled.save
+    cancelled
+  end
+
 
   before do
+    ticket_await_customer.state = State.find_by(name: "Waiting for Customer")
+    ticket_await_customer.save
+
+    ticket_completed.state = State.find_by(name: "Completed")
+    ticket_completed.save
+
+    ticket_on_hold.state = State.find_by(name: "On Hold")
+    ticket_on_hold.save
+
     visit '/'
     click_link "Log in"
     fill_in "Name", with: "david"
@@ -29,28 +79,45 @@ describe "Viewing Tickets" do
     expect(page).to have_selector('.content.active#panel-1')
 
     within('.content#panel-1') do
-      expect(page).to     have_content('New unassigned tickets')
-      expect(page).to_not have_content('Open Tickets')
-      expect(page).to_not have_content('On-hold Tickets')
-      expect(page).to_not have_content('Closed Tickets')
+      expect(page).to     have_content('These are tickets not yet assigned to a staff member.')
+
+      expect(page).to     have_content("#{ticket_await_staff.reference_id}")
+
+      expect(page).to_not have_content("#{ticket_on_hold.reference_id}")
+      expect(page).to_not have_content("#{ticket_cancelled.reference_id}")
+      expect(page).to_not have_content("#{ticket_completed.reference_id}")
     end
   end
 
   scenario "Open Tickets" do
     within('.content#panel-2') do
-      expect(page).to     have_content('Open Tickets')
+      expect(page).to     have_content("#{ticket_await_customer.reference_id}")
+
+      expect(page).to_not have_content("#{ticket_on_hold.reference_id}")
+      expect(page).to_not have_content("#{ticket_cancelled.reference_id}")
+      expect(page).to_not have_content("#{ticket_completed.reference_id}")
     end
   end
 
   scenario "On-hold Tickets" do
     within('.content#panel-3') do
-      expect(page).to     have_content('On-hold Tickets')
+      expect(page).to     have_content("#{ticket_on_hold.reference_id}")
+
+      expect(page).to_not have_content("#{ticket_await_staff.reference_id}")
+      expect(page).to_not have_content("#{ticket_await_customer.reference_id}")
+      expect(page).to_not have_content("#{ticket_cancelled.reference_id}")
+      expect(page).to_not have_content("#{ticket_completed.reference_id}")
     end
   end
 
   scenario "Closed Tickets" do
     within('.content#panel-4') do
-      expect(page).to     have_content('Closed Tickets')
+      expect(page).to     have_content("#{ticket_cancelled.reference_id}")
+      expect(page).to     have_content("#{ticket_completed.reference_id}")
+
+      expect(page).to_not have_content("#{ticket_on_hold.reference_id}")
+      expect(page).to_not have_content("#{ticket_await_customer.reference_id}")
+      expect(page).to_not have_content("#{ticket_await_staff.reference_id}")
     end
   end
 end
