5302af89138f0c5ab17851e412af3e2c78df1441
Digital-Learner <digital.learning.journeys@gmail.com>
2014-09-18 00:33:36 +0100

    Add validation to comments
    - comment must be present & be > 4 characters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :ticket
   belongs_to :user
+
+  validates :comment, presence: true
+  validates :comment, length: { minimum: 4 }
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Comment, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let!(:valid_comment) { Comment.create(comment: "Four") }
+
+  subject { valid_comment }
+
+  it { should respond_to(:comment) }
+
+  it { should be_valid }
+
+  describe "when comment is empty" do
+    before { subject.comment = "" }
+
+    it { should_not be_valid }
+  end
+
+  describe "when too short ( < 4 characters)" do
+    before { subject.comment = "A" }
+
+    it { should_not be_valid }
+  end
 end
